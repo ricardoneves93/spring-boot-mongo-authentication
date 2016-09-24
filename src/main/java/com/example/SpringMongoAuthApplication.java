7fe7ac128ddbd45734a74aff524edd42cfdf1972
Renjith G R <grenjithr@gmail.com>
2016-09-24 17:09:16 +0530

    Adding spring project

---
+++ src/main/java/com/example/SpringMongoAuthApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoAuthApplication.class, args);
+	}
+}
