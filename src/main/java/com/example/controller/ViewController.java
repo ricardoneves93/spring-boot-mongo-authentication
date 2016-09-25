ea95aa741fd42d0c1dbc36d53b93928c22e4992f
Renjith G R <grenjithr@gmail.com>
2016-09-25 13:23:18 +0530

    Polish

--- src/main/java/com/example/controller/ViewController.java
+++ src/main/java/com/example/controller/ViewController.java
@@ -3,25 +3,22 @@ package com.example.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ViewController {
 
-    @RequestMapping(name = "index", value = "/", method = RequestMethod.GET)
-    public ModelAndView showHomePage() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("index");
-        return modelAndView;
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String showHomePage() {
+        return "index";
     }
 
-    @RequestMapping(value = "/login")
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String showLoginPage() {
         return "login";
     }
 
     @RequestMapping(value = "/signup", method = RequestMethod.GET)
-    public String showSignUp() {
+    public String showSignUpPage() {
         return "signup";
     }
 }
